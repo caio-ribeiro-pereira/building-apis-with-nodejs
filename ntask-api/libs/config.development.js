97b3c862a5ea8316126500213c9a58a97414455d
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-02-18 12:06:35 -0200

    implementing chapter 11

--- ntask-api/libs/config.development.js
+++ ntask-api/libs/config.development.js
@@ -1,3 +1,5 @@
+import logger from "./logger.js";
+
 module.exports = {
   database: "ntask",
   username: "",
@@ -5,6 +7,9 @@ module.exports = {
   params: {
     dialect: "sqlite",
     storage: "ntask.sqlite",
+    logging: (sql) => {
+      logger.info(`[${new Date()}] ${sql}`);
+    },
     define: {
       underscored: true
     }
--- ntask-api/libs/middlewares.js
+++ ntask-api/libs/middlewares.js
@@ -1,9 +1,24 @@
 import bodyParser from "body-parser";
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import logger from "./logger.js";
 
 module.exports = app => {
   app.set("port", 3000);
   app.set("json spaces", 4);
+  app.use(morgan("common", {
+    stream: {
+      write: (message) => {
+        logger.info(message);
+      }
+    }
+  }));
+  app.use(cors({
+    origin: ["http://localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
